867edf43c4a74459eac6c42b8d35b13d69685066
edcas <edcasdevelopment@gamail.com>
2020-07-14 00:59:26 -0500

    Se actualiza base de datos con Firebase Base de datos para formulario de contacto funciona a la perfecction, se añade funcion para registrar la visita de un usuario cada vez que cargue la pagina

---
+++ scripts/contacto.js
@@ -0,0 +1,128 @@
+ // Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBq-VhsTf1ip5Le7FDIAXJA-qBpQ_91W4g",
+    authDomain: "contacto-de-usuarios.firebaseapp.com",
+    databaseURL: "https://contacto-de-usuarios.firebaseio.com",
+    projectId: "contacto-de-usuarios",
+    storageBucket: "contacto-de-usuarios.appspot.com",
+    messagingSenderId: "38625858230",
+    appId: "1:38625858230:web:44ad53d6d12d043d134c17",
+    measurementId: "G-0GSH2MDHRG"
+};
+  // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+var firestore = firebase.firestore();
+
+var db = firestore.collection("Contacto");
+var dbVisitantes = firestore.collection("Visitante");
+
+//accediendo a los elementos del formulario
+var nombre = document.getElementById('name');
+var telefono = document.getElementById('telefono');
+var email = document.getElementById('email');
+var mensaje = document.getElementById('mensaje');
+
+var formulario = document.getElementById('formularioContacto');
+var mensajeError = document.getElementById('mensajesFormulario');
+var mensajeErrorGrave = document.getElementById('mensajesFormularioGrave');
+var mensajeErrorExitoso = document.getElementById('mensajeExitoso');
+formulario.addEventListener('submit', function(evt){
+	evt.preventDefault();	
+	let nombreUsuario = nombre.value;
+	let telefonoUsuario = telefono.value;
+	let emailUsuario = email.value;
+	let mensajeUsuario = mensaje.value;
+	let fechaMensaje = new Date();
+	
+	// validar datos del formulario
+	if(nombreUsuario === null || nombreUsuario === ''){
+		mensajeError.classList.add('error');
+		mensajeError.innerHTML = '¡Ups! El campo "Nombre" es requerido.';
+		nombre.style.border = '2px solid #ff0000';
+	}
+	else if(telefonoUsuario === null || telefonoUsuario === ''){
+		mensajeError.classList.add('error');
+		mensajeError.innerHTML = '¡Ups! El campo "Télefono" es requerido.';
+		telefono.style.border = '2px solid #ff0000';
+		nombre.style.border = 'none';
+	}
+	else if(isNaN(telefonoUsuario)){
+		mensajeError.classList.add('error');
+		mensajeError.innerHTML = '¡Ups! El campo "Télefono" debe contener un número.';
+		telefono.style.border = '2px solid #ff0000';
+		nombre.style.border = 'none';
+	}
+	else{	
+	    db.doc().set({
+			a_nombre : nombreUsuario,
+			b_télefono : telefonoUsuario,
+			c_correo : emailUsuario,
+			d_mensaje : mensajeUsuario,
+			e_fechaMensaje : fechaMensaje,
+		})
+		.then(function(){
+			console.log('Data Saved');// resetando los campos...// Confirmacion de registro
+		        mensajeError.classList.add('EnvioExitoso');
+		        mensajeError.style.color = '#202020'; 
+				mensajeErrorExitoso.innerHTML = '&#10004;'; 
+				mensajeError.innerHTML = '¡Gracias ' + nombreUsuario + 
+		                         '! me pondré en contacto contigo en el menor tiempo posible al número ' + 
+								 telefonoUsuario + ' .' + ' Si este número no es correcto ingresalo de nuevo.';
+	        nombre.value = '';
+	        telefono.value = '';
+	        email.value = '';
+	        mensaje.value = '';
+		    nombre.style.border = 'none';
+		    telefono.style.border = 'none';
+		})
+		.catch(function(error){
+			console.log(error);
+		        mensajeError.classList.add('error');
+				mensajeErrorGrave.innerHTML = '&#9888; '; 
+				mensajeError.innerHTML = '¡Oh ' + nombreUsuario + 
+		                         '! Se ha presentado un inconveniente inesperado, '+
+								 ' por favor escribeme al whatsapp 3148582037 o '+
+								 'al correo alejacastanofotografia@gmail.com, '+
+								 'anexa tus datos y te contactaré en el menor tiempo posible.';
+	        nombre.value = '';
+	        telefono.value = '';
+	        email.value = '';
+	        mensaje.value = '';
+		    nombre.style.border = 'none';
+		    telefono.style.border = 'none';
+		});
+
+    setTimeout(function(){
+		mensajeError.style.display = 'none';
+		mensajeErrorExitoso.style.display = 'none';
+		mensajeErrorGrave.style.display = 'none';
+	}, 60000);
+
+        
+        
+	}
+	
+});
+function nuevoVisitante(){
+	alert('Soy un nuevo visitante');
+	let fecha = new Date();
+	let width = screen.width;
+	let height = screen.height;
+	let widthDisponible = screen.availWidth;
+	let heightDisponible = screen.availHeight;
+	
+	dbVisitantes.doc().set({
+		a_fecha : fecha,
+		a_width : width,
+		a_height : height,
+		b_widthDisponible : widthDisponible,
+		b_heightDisponible : heightDisponible,
+	})
+	.then(function(){
+		console.log('nuevo user');
+	})
+	.catch(function(error){
+		console.log(error);
+	})
+}
